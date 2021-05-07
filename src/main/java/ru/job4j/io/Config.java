b2c75ffef91bcb2bcd63897f3867329d8df6e922
FaizievRT <rus23f@mail.ru>
2021-05-07 21:48:02 +0300

    1. Читаем файл конфигурации [#488199]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,52 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+    private final String path;
+    private final Map<String, String> values = new HashMap<String, String>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader in = new BufferedReader(new FileReader(path))) {
+            in.lines().filter(x -> !x.contains("#") && x.length() > 0)
+                    .forEach(elm -> {
+                        String[] key = elm.split("=");
+                        if (key.length != 2) {
+                            throw new IllegalArgumentException();
+                        }
+                        values.put(key[0], key[1]);
+                    });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().filter(x -> !x.contains("#") && x.length() > 0).forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("src\\main\\resources\\Config1.properties"));
+    }
+
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class ConfigTest {
+
+    @Test
+    public void test1() {
+        String path = "src\\main\\resources\\Config1.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name1"), is("Petr Arsentev"));
+        assertThat(config.value("name3"), is("Medvedev Dimon"));
+    }
+
+    @Test
+    public void test2() {
+        String path = "src\\main\\resources\\Config1.properties";
+        Config config = new Config(path);
+        config.load();
+        assertNull(config.value("name4"));
+    }
+}
