f4dad7eb03abce5ab2196660328899bb9dc024de
FaizievRT <rus23f@mail.ru>
2021-05-16 21:50:07 +0300

    4.2. Поиск дубликатов [#488198] v2

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -4,20 +4,17 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    List<FileProperty> lstFileProp = new ArrayList<>();
+    HashSet<FileProperty> setFileProp = new HashSet<>();
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
         String tmpName = file.toFile().getName();
         long tmpSize = file.toFile().length();
         FileProperty fileProperty = new FileProperty(tmpSize, tmpName);
-        if (lstFileProp.contains(fileProperty)) {
+        if (!setFileProp.add(fileProperty)) {
             System.out.println("Обнаружен дуюлирующися файл: " + file.toAbsolutePath());
-        } else if (!lstFileProp.contains(fileProperty)) {
-            lstFileProp.add(fileProperty);
         }
         return FileVisitResult.CONTINUE;
     }
