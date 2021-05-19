ba04b7c61c14e9a3234101c8b80bd2439e955470
FaizievRT <rus23f@mail.ru>
2021-05-20 01:00:08 +0300

    1. Бот [#7921] v1

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -15,13 +15,24 @@ public class EchoServer {
                     String str;
                     while (!(str = in.readLine()).isEmpty()) {
                         System.out.println(str);
-                        if (str.contains("?msg=Bye")) {
+                        if (str.contains("msg=Exit")) {
+                            out.write("HTTP/1.1 200 OK\r\n".getBytes());
                             server.close();
                             break;
                         }
+                        if (str.contains("msg=Hello")) {
+                            out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                            out.write("Hello\r\n\r\n".getBytes());
+                            break;
+                        } else if (str.contains("msg=Any")) {
+                            out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                            out.write("What\r\n\r\n".getBytes());
+                            break;
+                        } else if (!str.contains("msg=Exit") && !str.contains("msg=Hello") && !str.contains("msg=Any")) {
+                            out.write("HTTP/1.1 200 OK\r\n".getBytes());
+                            break;
+                        }
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    out.write("Привет друг".getBytes());
                 }
             }
         }
