7d23543cc2529af4468fcecea2196541baec911b
FaizievRT <rus23f@mail.ru>
2021-05-20 23:24:58 +0300

    6. Кодировка. [#862 #61251] v2

--- src/main/java/ru/job4j/io/ConsoleChat.java
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -21,38 +22,44 @@ public class ConsoleChat {
 
     public void run() throws IOException {
         List<String> lstText = Files.readAllLines(Paths.get(botAnswers), Charset.forName("UTF-8"));
-        try (BufferedWriter out = new BufferedWriter(new FileWriter(path, Charset.forName("WINDOWS-1251"), true))) {
-            Random random = new Random();
-            System.out.println("-------------------------------------------------------");
-            System.out.println("Пробуй вводить текст:");
-            System.out.println("-------------------------------------------------------");
-            out.write("-------------------------------------------------------" + System.lineSeparator());
-            out.write("Пробуй вводить текст:" + System.lineSeparator());
-            out.write("-------------------------------------------------------" + System.lineSeparator());
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            String inputText = "";
-            String statusRequest = "продолжить";
-            while (!inputText.contains(OUT)) {
-                inputText = reader.readLine();
-                out.write(inputText + System.lineSeparator());
-                if (inputText.contains(OUT)) {
-                    break;
-                } else if (statusRequest.contains(CONTINUE) && !inputText.contains(STOP)) {
-                    String request = lstText.get(random.nextInt(lstText.size() - 1));
-                    out.write(request + "(ОТВЕТ)" + System.lineSeparator());
-                    System.out.println(request);
-                } else if (inputText.contains(STOP)) {
-                    statusRequest = STOP;
-                } else if (inputText.contains(CONTINUE)) {
-                    statusRequest = CONTINUE;
-                }
+        List<String> lstLog = new ArrayList<>();
+        Random random = new Random();
+        System.out.println("-------------------------------------------------------");
+        System.out.println("Пробуй вводить текст:");
+        System.out.println("-------------------------------------------------------");
+        lstLog.add("-------------------------------------------------------");
+        lstLog.add("Пробуй вводить текст:");
+        lstLog.add("-------------------------------------------------------");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String inputText = "";
+        String statusRequest = "продолжить";
+        while (!inputText.contains(OUT)) {
+            inputText = reader.readLine();
+            lstLog.add(inputText);
+            if (inputText.contains(OUT)) {
+                break;
+            } else if (statusRequest.contains(CONTINUE) && !inputText.contains(STOP)) {
+                String request = lstText.get(random.nextInt(lstText.size() - 1));
+                lstLog.add(request);
+                System.out.println(request);
+            } else if (inputText.contains(STOP)) {
+                statusRequest = STOP;
+            } else if (inputText.contains(CONTINUE)) {
+                statusRequest = CONTINUE;
             }
-            out.write("-------------------------------------------------------" + System.lineSeparator());
-            out.write("Завершение сеанса чата" + System.lineSeparator());
-            out.write("-------------------------------------------------------" + System.lineSeparator());
+        }
+        lstLog.add("-------------------------------------------------------");
+        lstLog.add("Завершение сеанса чата");
+        lstLog.add("-------------------------------------------------------");
+        try (BufferedWriter out = new BufferedWriter(new FileWriter(path, Charset.forName("WINDOWS-1251"), true))) {
+            for (var line : lstLog) {
+                out.write(line);
+                out.newLine();
+            }
+
         } catch (Exception e) {
-        e.printStackTrace();
-    }
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) throws IOException {
