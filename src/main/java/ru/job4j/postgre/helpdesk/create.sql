9e97de5ffa32a653933af3a12a2f381b24a1bd38
FaizievRT <rus23f@mail.ru>
2021-06-20 00:51:25 +0300

    1. Схема прав пользователи и роли. [#1093 #168516] v2

--- src/main/java/ru/job4j/postgre/helpdesk/create.sql
+++ src/main/java/ru/job4j/postgre/helpdesk/create.sql
@@ -9,32 +9,19 @@ create table rules(
 
 create table role(
     id serial primary key,
-    name_role varchar(255),
-    id_rules_role int references rules(id)
+    name_role varchar(255)
+);
+
+cerate table rules_role(
+    id serial primary key,
+    id_rules int references rules(id)
+    id_role int references role(id)
 );
 
 create table users(
     id serial primary key,
     name_user varchar(255),
     id_role_user int references role(id)
-);
-
-create table item(
-    id serial primary key,
-    name_item varchar(255),
-    id_users_item int references users(id)
-);
-
-create table comments(
-    id serial primary key,
-    comment text,
-    id_item_comment int references item(id)
-);
-
-create table attaches(
-    id serial primary key,
-    path_file text,
-    id_item_attach int references item(id)
 );
 
 create table category(
@@ -47,9 +34,22 @@ create table state(
     name_state varchar(255)
 );
 
-create table item_category_state(
+create table item(
     id serial primary key,
-    item_id int references item(id),
-    category_id int references category(id),
-    state_id int references state(id)
+    name_item varchar(255),
+    id_users_item int references users(id),
+    id_category_item int references category(id),
+    id_state_item int references state(id)
+);
+
+create table comments(
+    id serial primary key,
+    comment text,
+    id_item_comment int references item(id)
+);
+
+create table attaches(
+    id serial primary key,
+    path_file text,
+    id_item_attach int references item(id)
 );
