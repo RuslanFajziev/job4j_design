f84f009c196ecafb9eaa6a885101eb1d7b8fa1ee
FaizievRT <rus23f@mail.ru>
2021-06-20 22:23:38 +0300

    Фильтры [#1732 #169399] v2

--- src/main/java/ru/job4j/postgre/students/test4.sql
+++ src/main/java/ru/job4j/postgre/students/test4.sql
@@ -31,7 +31,7 @@ select * from product where type_id in (select id from type where name = 'Сыр
 -- 2 --
 select * from product where type_id in (select id from type where name like '%Мороженное%');
 -- 3 --
-select * from product where expired_date < '2021-06-19';
+select * from product where expired_date < current_date;
 -- 4 --
 select pr.name as "Самый дорогой продукт", pr.price цена from product as pr where pr.price in (select max(price) from product);
 -- 5 --
@@ -39,11 +39,18 @@ select tp.name, count(*) from product as pr
 join type as tp on tp.id = pr.type_id
 GROUP BY tp.name;
 -- 6 --
-select * from product where type_id in (1, 2);
+select * from product where type_id in (select id from type where name = 'Сыр' or name = 'Молоко');
 -- 7 --
-select quantity.name, quantity.count from (select tp.name, count(*) from product as pr
+select quantity.name, quantity.count from(select tp.name, count(*) from product as pr
 join type as tp on tp.id = pr.type_id
 GROUP BY tp.name) as quantity where quantity.count < 10;
+
+select tp.name, count(*) from product as pr
+join type as tp on tp.id = pr.type_id
+GROUP BY tp.name
+having count(*) < 10;
 -- 8 --
 select tp.name as "Тип продукта", pr.name as "Название продукта" from product as pr
 join type as tp on tp.id = pr.type_id;
+
+select current_date
