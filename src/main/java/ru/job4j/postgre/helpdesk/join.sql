8a282f9b909a3293ceba3a6572fd7f6d1df3956d
FaizievRT <rus23f@mail.ru>
2021-06-21 22:20:53 +0300

    0. Join [#6862 #169728] v2

--- src/main/java/ru/job4j/postgre/helpdesk/join.sql
+++ src/main/java/ru/job4j/postgre/helpdesk/join.sql
@@ -19,7 +19,7 @@ insert into emploees(name, id_departments) values('Sidorov', 1);
 insert into emploees(name, id_departments) values('Bulba', 3);
 insert into emploees(name, id_departments) values('Cipko', 3);
 
--- 2 --
+-- 2  Выполнить запросы с left, rigth, full, cross соединениями --
 select * from departments as dp
 left join emploees as emp on emp.id_departments = dp.id;
 select * from emploees as emp
@@ -34,11 +34,11 @@ select * from departments as dp
 cross join emploees as emp;
 select * from emploees as emp
 cross join departments as dp;
--- 3 --
+-- 3 Используя left join найти департаменты, у которых нет работников --
 select * from departments as dp
 left join emploees as emp on emp.id_departments = dp.id
 where emp.id_departments is null;
--- 4 --
+-- 4 Используя left и right join написать запросы, которые давали бы одинаковый результат --
 select * from departments as dp
 left join emploees as emp on emp.id_departments = dp.id;
 select * from emploees as emp
@@ -48,23 +48,20 @@ select * from emploees as emp
 left join departments as dp on emp.id_departments = dp.id;
 select * from departments as dp
 right join emploees as emp on emp.id_departments = dp.id;
--- 5 --
+-- 5 Создать таблицу teens с атрибутами name, gender и заполнить ее. Используя cross join составить все возможные разнополые пары --
 create table teens(
     id serial primary key,
-    name varchar(255)
+    name varchar(255),
+    gender varchar(255)
 );
 
-create table gender(
-    id serial primary key,
-    name varchar(255)
-);
+insert into teens(name, gender) values('Petr', 'mens');
+insert into teens(name, gender) values('Vasay', 'mens');
+insert into teens(name, gender) values('Nikolay', 'mens');
+insert into teens(name, gender) values('Irina', 'womens');
+insert into teens(name, gender) values('Lera', 'womens');
+insert into teens(name, gender) values('Natasha', 'womens');
 
-insert into teens(name) values('teans 1');
-insert into teens(name) values('teans 2');
-insert into teens(name) values('teans 3');
-
-insert into gender(name) values('mens');
-insert into gender(name) values('womens');
-
-select * from teens
-cross join gender;
+select tn.name, tnR.name from teens as tn
+cross join teens as tnR
+where tn.gender <> tnR.gender;
