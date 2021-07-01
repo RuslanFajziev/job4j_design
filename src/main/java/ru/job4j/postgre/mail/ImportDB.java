be507d3c0f5e86f70d9ea1e3bb4b78669c703286
FaizievRT <rus23f@mail.ru>
2021-07-02 01:03:49 +0300

    3. Загрузка базы спамеров. [#20459]

---
+++ src/main/java/ru/job4j/postgre/mail/ImportDB.java
@@ -0,0 +1,73 @@
+package ru.job4j.postgre.mail;
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class ImportDB {
+
+    private Properties cfg;
+    private String dump;
+
+    public ImportDB(Properties cfg, String dump) {
+        this.cfg = cfg;
+        this.dump = dump;
+    }
+
+    public List<User> load() throws IOException {
+        List<User> users = new ArrayList<>();
+        try (BufferedReader rd = new BufferedReader(new FileReader(dump))) {
+            rd.lines().filter(x -> x.length() > 0 && !x.contains("#"))
+                    .forEach(elm -> {
+                        String[] spammers = elm.substring(0, elm.length() - 1).split(";");
+                        if (spammers.length != 2) {
+                            throw new IllegalArgumentException();
+                        }
+                        users.add(new User(spammers[0], spammers[1]));
+                    });
+        }
+        return users;
+    }
+
+    public void save(List<User> users) throws ClassNotFoundException, SQLException {
+        Class.forName(cfg.getProperty("jdbc.driver"));
+        try (Connection cnt = DriverManager.getConnection(
+                cfg.getProperty("jdbc.url"),
+                cfg.getProperty("jdbc.username"),
+                cfg.getProperty("jdbc.password")
+        )) {
+            for (User user : users) {
+                try (PreparedStatement ps = cnt.prepareStatement("insert into users(name, email) values(?, ?)")) {
+                    ps.setString(1, user.name);
+                    ps.setString(2, user.email);
+                    ps.execute();
+                }
+            }
+        }
+    }
+
+    private static class User {
+        String name;
+        String email;
+
+        public User(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        Properties cfg = new Properties();
+        try (FileInputStream in = new FileInputStream("./src/main/resources/ImportDB.properties")) {
+            cfg.load(in);
+        }
+        ImportDB db = new ImportDB(cfg, "./src/main/resources/dumpSpammers.txt");
+        db.save(db.load());
+    }
+}
---
+++ src/test/java/ru/job4j/postgre/mail/ImportDBTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.postgre.mail;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class ImportDBTest {
+    @Test
+    public void test() throws Exception {
+        List<String> left = List.of("Petr Arsentev", "Ivan Ivanov");
+        List<String> right = new ArrayList<>();
+        Properties cfg = new Properties();
+        try (FileInputStream in = new FileInputStream("./src/main/resources/ImportDB.properties")) {
+            cfg.load(in);
+        }
+        ImportDB db = new ImportDB(cfg, "./src/main/resources/dumpSpammers.txt");
+        db.save(db.load());
+        Class.forName(cfg.getProperty("jdbc.driver"));
+        try (Connection cnt = DriverManager.getConnection(
+                cfg.getProperty("jdbc.url"),
+                cfg.getProperty("jdbc.username"),
+                cfg.getProperty("jdbc.password")
+        )) {
+            try (PreparedStatement ps = cnt.prepareStatement("select name from users")) {
+                try (ResultSet resultSet = ps.executeQuery()) {
+                    while (resultSet.next()) {
+                        right.add(resultSet.getString(1));
+                    }
+                }
+            }
+        }
+        assertThat(left.get(0), is(right.get(0)));
+        assertThat(left.get(1), is(right.get(1)));
+    }
+}
