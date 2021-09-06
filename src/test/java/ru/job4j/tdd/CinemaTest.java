b68a6061a2dedf61b77ed1fdc39bb56dc4f37f19
FaizievRT <rus23f@mail.ru>
2021-09-07 00:11:55 +0300

    2. Что такое TDD? v2

--- src/test/java/ru/job4j/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -22,13 +22,21 @@ public class CinemaTest {
     }
 
     @Test(expected = Exception.class)
-    public void buyErr() {
+    public void buyErrTicket() {
         Account account = new AccountCinema();
         Cinema cinema = new Cinema3D();
         Calendar date = Calendar.getInstance();
         date.set(2020, 10, 10, 23, 00);
         Ticket ticket = cinema.buy(account, 1, 1, date);
         ticket = cinema.buy(account, 1, 1, date);
+    }
+
+    @Test(expected = Exception.class)
+    public void buyErrDate() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 13, 10, 23, 00);
     }
 
     @Test
@@ -46,5 +54,14 @@ public class CinemaTest {
         cinema.add(new Session3D());
         List<Session> sessions = cinema.find(session -> true);
         assertThat(sessions, is(Arrays.asList(new Session3D())));
+    }
+
+    @Test
+    public void add() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> true);
+        assertThat(sessions, is(Arrays.asList(new Session3D(), new Session3D())));
     }
 }
