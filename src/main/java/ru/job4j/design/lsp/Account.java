134f2abafc5910df041fac83bcb42f9c04a15acf
FaizievRT <rus23f@mail.ru>
2021-09-20 01:23:57 +0300

    0. Принцип подстановки Лиско v2

--- src/main/java/ru/job4j/design/lsp/Account.java
+++ src/main/java/ru/job4j/design/lsp/Account.java
@@ -2,11 +2,20 @@ package ru.job4j.design.lsp;
 
 public class Account {
     int capital;
+    int result;
 
     public void setCapital(int capital) {
-        if (capital >= 0) {
+        if (capital <= 0) {
             throw new IllegalArgumentException("Начальный капитал не может быть 0 или отрицательным!");
         }
         this.capital = capital;
+    }
+
+    public int getResult() {
+        result = this.capital / 2;
+        if (result == 50) {
+            throw new IllegalArgumentException("куыгде не должен ровняться 50");
+        }
+        return result;
     }
 }
--- src/main/java/ru/job4j/design/lsp/MicroAccount.java
+++ src/main/java/ru/job4j/design/lsp/MicroAccount.java
@@ -3,11 +3,23 @@ package ru.job4j.design.lsp;
 public class MicroAccount extends Account {
     @Override
     public void setCapital(int capital) {
-        if (capital >= 0) {
+        if (capital <= 0) {
             throw new IllegalArgumentException("Начальный капитал не может быть 0 или отрицательным!");
         } else if (capital > 100) {
             throw new IllegalArgumentException("Начальный капитал не может быть более 100");
         }
         this.capital = capital;
+    }
+
+    @Override
+    public int getResult() {
+        if (this.capital == 100) {
+            this.capital += 10;
+        }
+        result = this.capital / 2;
+        if (result == 50) {
+            throw new IllegalArgumentException("куыгде не должен ровняться 50");
+        }
+        return result;
     }
 }
