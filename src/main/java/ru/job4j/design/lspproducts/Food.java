11e681b409fe79661077096831f42c29bf06efd8
FaizievRT <rus23f@mail.ru>
2021-09-22 10:30:32 +0300

    1. Хранилище продуктов v3

--- src/main/java/ru/job4j/design/lspproducts/Food.java
+++ src/main/java/ru/job4j/design/lspproducts/Food.java
@@ -4,11 +4,11 @@ import java.util.Date;
 import java.util.Objects;
 
 public class Food {
-    String name;
-    Date expiryDate;
-    Date createDate;
-    int price;
-    int discount;
+    private String name;
+    private Date expiryDate;
+    private Date createDate;
+    private int price;
+    private int discount;
 
     public Food(String name, Date expiryDate, Date createDate, int price, int discount) {
         this.name = name;
@@ -22,40 +22,20 @@ public class Food {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Date getExpiryDate() {
         return expiryDate;
-    }
-
-    public void setExpiryDate(Date expiryDate) {
-        this.expiryDate = expiryDate;
     }
 
     public Date getCreateDate() {
         return createDate;
     }
 
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-
     public int getPrice() {
         return price;
     }
 
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
     public int getDiscount() {
         return discount;
-    }
-
-    public void setDiscount(int discount) {
-        this.discount = discount;
     }
 
     @Override
